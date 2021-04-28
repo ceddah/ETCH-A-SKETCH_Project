413a9a8bf1a09c4b8d935805a2570d54736242e7
ceddah <cedanik1@gmail.com>
2021-04-28 21:26:09 +0200

    HTML/CSS Done

---
+++ script.js
@@ -0,0 +1,10 @@
+// Size of the DIV is containers width or height / n (n is number entered) - 30 for borders
+// Whem soneone enters E.G: 4 we want to make 4 * 4 boxes 
+
+const drawPad = document.querySelector('#drawPad');
+
+const numberOfBoxes = 16;
+
+function fillOutPad(size) {
+
+}
