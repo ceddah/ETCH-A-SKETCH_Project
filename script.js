58aa5dfe3088145ad331569ff760b4035f887cfe
ceddah <cedanik1@gmail.com>
2021-04-28 22:34:46 +0200

    /

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Size of the DIV is containers width or height / n (n is number entered) - 30 for borders
 // Whem soneone enters E.G: 4 we want to make 4 * 4 boxes 
+// Make a function for drawing on pad, and that function takes parameter for Color and we will
+// set default color to black and then when we change color we will call that function with
+// input:color value as parameter.
 
+//Maybe ADD A BUTTON FOR HIDING TOOL BAR and write Toolbar in h3 in tools section
 const drawPad = document.querySelector('#drawPad');
 
 const numberOfBoxes = 16;
